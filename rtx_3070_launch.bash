a6fc9c7f2333e8499dd2b9f439029bc1a8d857fc
Martin Ethier <methier@edu.uwaterloo.ca>
2021-02-01 00:17:26 -0500

    Added script to launch container with discord bot

---
+++ rtx_3070_launch.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./docker_run.bash -c config/rtx_3070.yaml -a discord -w https://discordapp.com/api/webhooks/805656512067272704/dRGD31nJzEh5UfoZmZnGRT91Gftvl4pi1lXdjw8Qak2TEWfx89pqMiYk5mkOcNo83zFB
